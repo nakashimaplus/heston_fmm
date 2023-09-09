131344e14785493f0ca8a028a3d3cf36e351074f
nakashimaplus <nakashimaplus@gmail.com>
2023-09-09 19:30:12 +0900

    libor_kについての機能追加

--- equity_hlmm.py
+++ equity_hlmm.py
@@ -1,14 +1,13 @@
 from math import (
-    exp,
     sqrt,
 )
 import numpy as np
 
 kappa = 1.2
-xi_bar = 0.01
+xi_bar = 0.1
 gamma = 0.5
 s_0 = 1
-xi_0 = 0.01
+xi_0 = 0.1
 beta = 0.5
 sigma = 0.25
 _lambda = 1
@@ -16,78 +15,135 @@ v_0 = 1
 eta = 0.1
 tau = 1
 
+terminal_n = 2
+maturity = 2
+strike = 2.4
+
 bond_list = [1, 0.9512, 0.9048, 0.8607, 0.8187,
              0.7788, 0.7408, 0.7047, 0.6703, 0.6376, 0.6065]
+
+
 delta_h = 1/20
 
-libor_0 = 1/0.9512-1
-
-N = 1
-
-rho = np.array([[1, -0.3,   0.5,    0],
-                [-0.3, 1,    0,  0],
-                [0.5,   0,  1,   0],
-                [0,  0,    0,   1]
-                ])
+rho_l_l = 0.98
 
 
 def generate_rho(n_number):
     rho_xi_x = np.array([[1, -0.3],
                          [-0.3, 1]])
 
+    rho_libor_libor = np.zeros((n_number, n_number))
+    for i in range(n_number):
+        for j in range(n_number):
+            rho_libor_libor[i, j] = 1 if i == j else rho_l_l
 
-def f_libor(libor_ini, v_ini, w_libor):
-    return libor_ini + sigma * (beta*libor_ini + (1-beta)*libor_0) * sqrt(abs(v_ini)) * sqrt(delta_h)*w_libor
+    rho_libor_x = np.array([0.5] * n_number)
+
+    rho_v = np.array([0]*(n_number+2))
+    rho_xi = np.array([0]*n_number)
+
+    c_1 = np.vstack((rho_libor_x, rho_xi))
+    c_2 = np.hstack((rho_xi_x, c_1))
+    c_3 = np.hstack((c_1.T, rho_libor_libor))
+    c_4 = np.vstack((c_2, c_3))
+    c_5 = np.vstack((c_4, rho_v))
+    c_6 = np.hstack((rho_v, [1]))
+    rho = np.vstack((c_5.T, c_6)).T
+
+    return rho
 
 
-def f_v(v_ini, w_v):
+def libor(k,  v_ini, libor_list, w_libor):
+    tmp1 = libor_list[k-1] + sigma * \
+        phi(k,  libor_list[k-1]) * sqrt(abs(v_ini)) * sqrt(delta_h)*w_libor
+    if k == terminal_n:
+        return tmp1
+
+    tmp2 = -phi(k,  libor_list[k-1]) * sigma * v_ini
+    sum = 0
+    for j in range(k+1, terminal_n+1):
+        tmp3 = tau * phi(j, libor_list=libor_list) * \
+            sigma / (1 + tau * libor_list[j-1]) * rho_l_l * delta_h
+        sum += tmp2 * tmp3
+
+    return sum + tmp1
+
+
+def v_libor(v_ini, w_v):
     return v_ini + _lambda * (v_0-v_ini)*delta_h + eta * sqrt(abs(v_ini))*sqrt(delta_h)*w_v
 
 
-def f_f(f_ini, xi_ini, v_ini, libor_ini, w_x, w_libor):
-    tmp1 = sqrt(abs(xi_ini))*sqrt(delta_h)*w_x
-    tmp2 = tau*sigma*(beta*libor_ini + (1-beta)*libor_0)
-    tmp3 = sqrt(abs(v_ini)) * sqrt(delta_h)*w_libor
-    tmp4 = 1 + tau * libor_ini
-    f = f_ini + f_ini * (tmp1 + tmp2 * tmp3 / tmp4)
-    return f
+def phi(k, libor_k=None, libor_list=None):
+    return beta * libor_k + (1 - beta) * libor_zero(k) if libor_k is not None else beta * libor_list[k-1] + \
+        (1 - beta) * libor_zero(k)
 
 
-def f_xi(xi_ini, w_xi):
+def libor_zero(k):
+    return 1 / tau * (bond_list[k-1] / bond_list[k]-1)
+
+
+def forward_equity(f_e_ini, xi_ini, v_ini, libor_list, w_x, w_libor_list):
+    tmp1 = sqrt(abs(xi_ini)) * sqrt(delta_h) * w_x
+    sum = 0
+    for j in range(0, terminal_n+1):
+        tmp2 = tau * sigma * phi(j, libor_list=libor_list) * sqrt(abs(v_ini))
+        tmp3 = 1 + tau * libor_list[j-1]
+        tmp4 = sqrt(delta_h) * w_libor_list[j-1]
+        sum += tmp2 / tmp3 * tmp4
+
+    f_e = f_e_ini + f_e_ini * (tmp1 + sum)
+
+    return f_e
+
+
+def xi_forward_equity(xi_ini, w_xi):
     return xi_ini + kappa*(xi_bar-xi_ini)*delta_h + gamma * sqrt(abs(xi_ini))*sqrt(delta_h)*w_xi
 
 
-def generate_rv():
+def generate_rv(rho):
     rho_tri = np.linalg.cholesky(rho)
-    w_x, w_xi, w_libor, w_v = rho_tri@np.random.normal(0, 1, N + 3)
+    w_list = rho_tri@np.random.normal(0, 1, terminal_n + 3)
+    w_x = w_list[0]
+    w_xi = w_list[1]
+    w_libor_list = w_list[2:terminal_n+2]
+    w_v = w_list[terminal_n+2]
 
-    return w_x, w_xi, w_libor, w_v
+    return w_x, w_xi, w_libor_list, w_v
 
 
-def calculate_equity(T, K):
+def calculate_equity(maturity, strike):
+    libor_list = [libor_zero(i) for i in range(1, terminal_n+1)]
+    rho = generate_rho(maturity)
 
-    grid_list = [grid*delta_h for grid in range(1, int(T/delta_h+1))]
+    grid_list = [grid*delta_h for grid in range(1, int(maturity/delta_h+1))]
 
     _v = v_0
-    _libor = 1/0.9512-1
     _xi = xi_0
-    _f = s_0 / bond_list[1]
+    _f = s_0 / bond_list[maturity]
     for grid in grid_list:
-        w_x, w_xi, w_libor, w_v = generate_rv()
-        _libor = f_libor(_libor, _v, w_libor)
-        _v = f_v(_v, w_v)
-        _f = f_f(_f, _xi, _v, _libor, w_x, w_libor)
-        _xi = f_xi(_xi, w_xi)
+        w_x, w_xi, w_libor_list, w_v = generate_rv(rho)
 
-    return max(_f-K, 0)
+        tmp_libor_list = []
+        for k in range(1, terminal_n+1):
+            tmp_libor_list.append(libor(k, _v, libor_list, w_libor_list[k-1]))
+
+        _f = forward_equity(_f, _xi, _v, libor_list, w_x, w_libor_list)
+        v_calc = v_libor(_v, w_v)
+        xi_calc = xi_forward_equity(_xi, w_xi)
+
+        libor_list = tmp_libor_list
+        _v = v_calc
+        _xi = xi_calc
+
+    return max(_f-strike, 0)
 
 
-def calculate_equity_loop(T, K):
-    _N = 50000
+def calculate_equity_loop(maturity, strike):
+    _N = 20000
     sum = 0
     for i in range(0, _N):
-        sum += calculate_equity(T, K)
-    return bond_list[1]*sum/_N
+        sum += calculate_equity(maturity, strike)
+    return bond_list[maturity] * sum / _N
 
 
 def beep(freq, dur=100):
@@ -97,10 +153,6 @@ def beep(freq, dur=100):
 
 
 if __name__ == "__main__":
-
-    # print(a)
-
-    fx = calculate_equity_loop(1, 0.4)
-
+    fx = calculate_equity_loop(maturity, strike)
     print(fx)
     beep(500, 2000)
